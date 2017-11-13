fb8c74481104dfb39685eebbd8199bda1d2a5489
Jim Lin <jimlintw922@gmail.com>
2017-11-13 17:31:08 +0800

    update to-etcd/heartbeat.sh

--- to-etcd/heartbeat.sh
+++ to-etcd/heartbeat.sh
@@ -5,5 +5,9 @@ INTERVAL=3
 while [ 1 ]
 do
     echo "epoch = $(date +%s)"
+    #
+    export ETCDCTL_API=3
+    etcdctl --endpoints=http://$ETCD_CLIENT_PORT_2379_TCP_ADDR:2379 put /node/foo $(date +%s)
+    #
     sleep $INTERVAL
 done
