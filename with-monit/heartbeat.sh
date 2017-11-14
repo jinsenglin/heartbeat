923058c28b26a814cbb171be51f21bb9e6d98621
Jim Lin <jimlintw922@gmail.com>
2017-11-14 23:52:21 +0800

    update heartbeat.sh

--- with-monit/heartbeat.sh
+++ with-monit/heartbeat.sh
@@ -3,11 +3,6 @@
 INTERVAL=3
 
 #
-function clean_up {
-    rm /tmp/heartbeat.pid
-    exit
-}
-trap clean_up SIGTERM SIGINT SIGHUP SIGTERM
 echo $$ | tee /tmp/heartbeat.pid
 #
 
@@ -16,7 +11,3 @@ do
     echo "epoch = $(date +%s)" | tee -a /tmp/heartbeat.log
     sleep $INTERVAL
 done
-
-#
-clean_up
-#
